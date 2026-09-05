b4857d565d00313ff01e9d34b96d5765e5a46372
clautenbacher <c.lautenbacher@gmail.com>
2026-09-05 11:12:59 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const SUPABASE_URL = 'https://uvolbvrzakcrhizhspgv.supabase.co';
-const SUPABASE_ANON_KEY = 
-
-'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxeWZrZG1zeHFweHZnYW9mZ21uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg0MjI2NjAsImV4cCI6MjEwMzk5ODY2MH0.4a-kBOXSPQPAgnYLGrH68_OTI1uD49-dHT4K8ZkAv_I';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2b2xidnJ6YWtjcmhpemhzcGd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgzMTc3ODAsImV4cCI6MjAyMzg5MzczMH0.TaHfzj6zUvjvQpuDzeUULbcxbM1tIAr_MqKM9cqKOvE';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
